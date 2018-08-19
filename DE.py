27168fa107b45aedf0623ee727a0dd933a189245
Alex Matskevych <Axeleik>
2018-08-19 21:42:55 +0200

    computed first part, have to check if right

--- DE.py
+++ DE.py
@@ -7,9 +7,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 from time import time
 import os
+import sys
 
 #debugging
-from debugging_funcs import de_normalize, show_img, norm_show
+#from debugging_funcs import de_normalize, show_img, norm_show
 
 
 def load_all_NN():
@@ -25,23 +26,35 @@ def load_all_NN():
 
     return resnet152,alexnet,squeezenet,vgg16,densenet,inception
 
-def load_imagenet(img_size=600):
+def load_imagenet(img_size=600,clear=False):
     import os
     import torchvision.datasets as datasets
+
+    print("loading imagenet_{}".format(img_size))
+
+    #load random images
+    if os.path.exists("../imagenet_{}.torch".format(img_size)) and not clear:
+        print("imagedata already exists...")
+        return torch.load("../imagenet_{}.torch".format(img_size))
+
     # Data loading code
-    valdir = os.path.join("../","val/")
+    if torch.cuda.is_available():
+        print("using cuda")
+        valdir = os.path.join("/net/hci-storage02/userfolders/amatskev/", "val/")
+    else:
+        valdir = os.path.join("../","val/")
 
     # normally, we would use this transformation for our pretrained nets
     # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     # use this transformation to have RGB values in [0, 255], transform as above later on
-    normalize = transforms.Normalize(mean=[0, 0, 0], std=[1./255., 1./255., 1./255.])
-
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
     val_dataset = datasets.ImageFolder(
         valdir,
         transforms.Compose([
-            transforms.RandomResizedCrop(227),
-            transforms.RandomHorizontalFlip(),
+            transforms.Scale(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             normalize,
         ]))
@@ -53,19 +66,26 @@ def load_imagenet(img_size=600):
     #no dataloader, we need shuffled samples but after that constant drawing from list (either this or update to pytorch 0.4.1)
     val_images = [val_dataset[i][0] for i in indices]
     val_targets = [val_dataset[i][1] for i in indices]
+
+    torch.save((val_images,val_targets), "../imagenet_{}.torch".format(img_size))
+
     return val_images,val_targets
 
 
 def perturbator_3001(NeuralNet ,images_targets, pop_size=400, max_iterations=100, f_param=0.5, criterium="paper",
-                     pixel_number=1):
+                     pixel_number=1,cuda=False):
 
     #assertions
     assert(f_param>=0 and f_param <= 2),"0<=F<=2 !"
     assert (criterium=="paper" or criterium=="over50" or criterium=="under0.1"), "No valid criterium! "
 
-
     # extract images and targets
     images,targets=images_targets
+    if cuda:
+        images=torch.stack(images).cuda()
+    else:
+        images=torch.stack(images)
+
 
     list_pert_samples=[]
     list_iterations=[]
@@ -83,11 +103,30 @@ def perturbator_3001(NeuralNet ,images_targets, pop_size=400, max_iterations=100
         data_time=time()
 
         # draw the coordinates and rgb-values of the agents from random
-        coords = np.random.random_integers(0,data.size()[-1]-1, (pop_size,pixel_number,2))
-        rgb = (np.random.normal(128,127,(pop_size,pixel_number, 3)) % 255).round()
+        coords = np.random.random_integers(0, data.size()[-1]-1, (pop_size,pixel_number,2))
+        rgb = np.array([(np.random.normal(0.5 / 0.229, 0.5 / 0.229, (pop_size, pixel_number, 1)) % 1 / 0.229) - (0.485 / 0.229),
+                        (np.random.normal(0.5 / 0.224, 0.5 / 0.224, (pop_size, pixel_number, 1)) % 1 / 0.224) - (0.456 / 0.224),
+                        (np.random.normal(0.5 / 0.225, 0.5 / 0.225, (pop_size, pixel_number, 1)) % 1 / 0.225) - (0.406 / 0.225)])[:, :, :, 0]\
+            .swapaxes(0,1).swapaxes(1,2)
+
+        # TODO: CLip in right interval with : if over max boundary--> minus max, if under min boundary--> plus max
+        over0 = rgb[:, :, 0] > (1 - 0.485) / 0.229
+        over1 = rgb[:, :, 1] > (1 - 0.456) / 0.224
+        over2 = rgb[:, :, 2] > (1 - 0.406) / 0.225
+        under0 = rgb[:, :, 0] < - 0.485 / 0.229
+        under1 = rgb[:, :, 1] < - 0.456 / 0.224
+        under2 = rgb[:, :, 2] < - 0.406 / 0.225
+
+        assert(over0.any() == False and
+               over1.any() == False and
+               over2.any() == False and
+               under0.any() == False and
+               under1.any() == False and
+               under2.any() == False), "rgb values over boundary!"
 
         #initialize scores for comparison fathers-sons
-        fitness_list=np.ones(pop_size)
+        fitness_list = np.ones(pop_size)
+
 
         # set iterator to zero
         iteration = 0
@@ -102,16 +141,24 @@ def perturbator_3001(NeuralNet ,images_targets, pop_size=400, max_iterations=100
                 data_purb=data.clone()
 
                 #replace rgbs on the x and y positions (coords) in data_purb with the new rgbs (rgb)
-                data_purb[:, coords[i, :, 0], coords[i, :, 1]] = torch.tensor(rgb[i].transpose(), dtype=torch.float) % 255
+                if cuda:
+                    data_purb[:, coords[i, :, 0], coords[i, :, 1]] = torch.cuda.FloatTensor(rgb[i].transpose())
+                else:
+                    data_purb[:, coords[i, :, 0], coords[i, :, 1]] = torch.FloatTensor(rgb[i].transpose())
 
-                #normalize the image for the network
-                data_purb[0] = (data_purb[0]/255. - 0.485) / 0.229
-                data_purb[1] = (data_purb[1]/255. - 0.456) / 0.224
-                data_purb[2] = (data_purb[2]/255. - 0.406) / 0.225
+
+                #data_purb[0] = (data_purb[0]/255. - 0.485) / 0.229
+                #data_purb[1] = (data_purb[1]/255. - 0.456) / 0.224
+                #data_purb[2] = (data_purb[2]/255. - 0.406) / 0.225
 
                 # softmax
-                score = soft(NeuralNet(data_purb.unsqueeze(0)).squeeze())
-                true_score = score[target]
+                if cuda:
+                    score = soft(NeuralNet(data_purb.unsqueeze(0).cuda()).squeeze()).type(torch.float16)
+
+                else:
+                    score = soft(NeuralNet(data_purb.unsqueeze(0)).squeeze())
+
+                true_score = score[target].cpu().detach().numpy()
 
                 #check if son is better than father
                 if true_score>fitness_list[i]:
@@ -132,19 +179,47 @@ def perturbator_3001(NeuralNet ,images_targets, pop_size=400, max_iterations=100
                 else:
                     fitness_list[i]=true_score
 
+            if found_candidate:
+                break
+
             #remember the father gen
             coords_fathers=coords.copy()
             rgb_fathers=rgb.copy()
 
             # DE update agents
-            random_numbers=np.array([np.random.choice(range(pop_size), 3 , replace=False) for i in range(pop_size)])
+            random_numbers=np.array([np.random.choice(range(pop_size), 3, replace=False) for i in range(pop_size)])
             coords = (coords[random_numbers[:,0]] + f_param * (coords[random_numbers[:,1]] + coords[random_numbers[:,2]])).\
                              astype(int) % data.size()[-1]
-            rgb = ((rgb[random_numbers[:,0]] + f_param * (rgb[random_numbers[:,1]] + rgb[random_numbers[:,2]])) % 255).round()
+            rgb = (rgb[random_numbers[:,0]] + f_param * (rgb[random_numbers[:,1]] + rgb[random_numbers[:,2]]))
 
+            over0 = rgb[:, :, 0] > (1 - 0.485) / 0.229
+            over1 = rgb[:, :, 1] > (1 - 0.456) / 0.224
+            over2 = rgb[:, :, 2] > (1 - 0.406) / 0.225
+            under0 = rgb[:, :, 0] < - 0.485 / 0.229
+            under1 = rgb[:, :, 1] < - 0.456 / 0.224
+            under2 = rgb[:, :, 2] < - 0.406 / 0.225
 
-            if found_candidate:
-                break
+            rgb[:, :, 0][over0] -= 1 / 0.229
+            rgb[:, :, 1][over1] -= 1 / 0.224
+            rgb[:, :, 2][over2] -= 1 / 0.225
+            rgb[:, :, 0][under0] += 1 / 0.229
+            rgb[:, :, 1][under1] += 1 / 0.224
+            rgb[:, :, 2][under2] += 1 / 0.225
+
+            #test, just so i know i didnt do anything wrong
+            over0 = rgb[:, :, 0] > (1 - 0.485) / 0.229
+            over1 = rgb[:, :, 1] > (1 - 0.456) / 0.224
+            over2 = rgb[:, :, 2] > (1 - 0.406) / 0.225
+            under0 = rgb[:, :, 0] < - 0.485 / 0.229
+            under1 = rgb[:, :, 1] < - 0.456 / 0.224
+            under2 = rgb[:, :, 2] < - 0.406 / 0.225
+
+            assert (over0.any() == False)
+            assert (over1.any() == False)
+            assert (over2.any() == False)
+            assert (under0.any() == False)
+            assert (under1.any() == False)
+            assert (under2.any() == False)
 
             iteration += 1
 
@@ -161,61 +236,113 @@ def perturbator_3001(NeuralNet ,images_targets, pop_size=400, max_iterations=100
 
 def reproduction():
     """reproduction of the paper scores"""
-
+    cuda=False
+    if torch.cuda.is_available():
+        cuda=True
     _, alexnet, _, _, _, _ = load_all_NN()
 
+    if cuda:
+        print("Using Cuda")
+        #cuda4 = torch.device('cuda:4')
+        alexnet.cuda()
+
     #if already saved
-    if os.path.exists("../reproduction_init.torch"):
+    if os.path.exists("../reproduction_init_cpu.torch"):
         print("path already exists,loading...")
-        pert_samples, iterations, data_cropped= torch.load("../reproduction_init.torch")
+        pert_samples, iterations, data_cropped= torch.load("../reproduction_init_cpu.torch")
 
     else:
-        data_cropped = load_imagenet(10)
-        pert_samples, iterations = perturbator_3001(alexnet, data_cropped, 100)
+        data_cropped = load_imagenet(600)
+        pert_samples, iterations = perturbator_3001(alexnet, data_cropped, 400, cuda=cuda)
 
-        torch.save((pert_samples,iterations,data_cropped),"../reproduction_init.torch")
+        torch.save((pert_samples,iterations,data_cropped),"../reproduction_init_gpu.torch")
 
-    torch.save(accuracy(alexnet,data_cropped), "../reproduction_accuracy.torch")
-    torch.save(extract_stats(alexnet, data_cropped, pert_samples), "../reproduction_stats.torch")
+        output = []
 
+        for i in pert_samples:
+            output.append(i.cpu())
+        pert_samples=output
+        torch.save((pert_samples,iterations,data_cropped),"../reproduction_init_cpu.torch")
+
+
+    accuracy_score, pred_right = accuracy(alexnet.cpu(),data_cropped)
+    torch.save(accuracy_score, "../reproduction_accuracy.torch")
+    torch.save(extract_stats(alexnet.cpu(), data_cropped, pert_samples, pred_right), "../reproduction_stats.torch")
 
 
 def accuracy(NeuralNet,vals):
     """calculates accuracy of the neural net with only our cropped batch of examples"""
+    #
+    print("Computing accuracy")
+    soft=nn.Softmax(dim=0).cuda()
+
     #extract images and targets
     samples_orig,targets_orig = vals
 
+    #transform data
+    data=torch.stack(samples_orig)
+    ## normalize the image for the network
+    #data[:,0] = (data[:,0] / 255. - 0.485) / 0.229
+    #data[:,1] = (data[:,1] / 255. - 0.456) / 0.224
+    #data[:,2] = (data[:,2] / 255. - 0.406) / 0.225
+
     #calculate scores
-    scores_of_orig_images=NeuralNet(torch.stack(samples_orig))
+    if torch.cuda.is_available():
+        NeuralNet.cuda()
+        scores_of_orig_images = NeuralNet(data.cuda()).cpu()
+    else:
+        scores_of_orig_images = soft(NeuralNet(data))
 
     #calculate percentage of right predictions
     accuracy=np.sum(scores_of_orig_images.argmax(1).detach().numpy() == targets_orig)/len(targets_orig)
+    right_preds = np.where(scores_of_orig_images.argmax(1).detach().numpy() == targets_orig)[0]
+    #wrong_preds = np.where(scores_of_orig_images.argmax(1).detach().numpy() != targets_orig)[0]
 
-    return accuracy
+    #probability_of_true_class = scores_of_orig_images[np.arange(len(targets_orig)), targets_orig]
+    #probability_of_true_class = probability_of_true_class[wrong_preds]
 
-def extract_stats(NeuralNet ,vals,pert_samples):
+    #max_probability = np.max(scores_of_orig_images.detach().numpy(), axis=0)
+    #max_probability = max_probability[wrong_preds]
+
+    print("accuracy: ", accuracy)
+    #print(probability_of_true_class)
+    #print(max_probability)
+    #print(max_probability-probability_of_true_class.detach().numpy())
+    return accuracy, right_preds
+
+def extract_stats(NeuralNet ,vals,pert_samples,pred_right):
     """extracts stats of the perturbed samples"""
 
     # extract targets
     targets_orig = vals[1]
 
-    #computing scores of all perturbed image samples
-    scores_of_pert_images=NeuralNet(torch.stack(pert_samples))
+    ## use only data, which were predicted right in the first place
+    #targets_orig = [targets_orig[i] for i in pred_right]
+    #pert_samples = [pert_samples[i] for i in pred_right]
 
-    #scores of the true targets in perturbated predictions
+    # computing scores of all perturbed image samples
+    scores_of_pert_images = NeuralNet(torch.stack(pert_samples))
+
+    # scores of the true targets in perturbated predictions
     target_scores_in_pert_pred = scores_of_pert_images[np.arange(len(targets_orig)), targets_orig]
 
-    #calculate which of the classes are bigger than the perdiction score for the target class
-    bigger_than_target_pert =scores_of_pert_images.detach().numpy() > np.matrix(target_scores_in_pert_pred.detach().numpy()).transpose()
+    # calculate which of the classes are bigger than the prediction score for the target class
+    bigger_than_target_pert = scores_of_pert_images.detach().numpy() > np.matrix(target_scores_in_pert_pred.detach().numpy()).transpose()
 
-    #calculate percentage of classes bigger than the target class score
-    success_rate=np.sum(bigger_than_target_pert.any(1))/len(targets_orig)
 
-    #calculate confidence (average probability of target classes)
-    confidence=np.sum(target_scores_in_pert_pred.detach().numpy())/len(target_scores_in_pert_pred)
+    # calculate percentage of classes bigger than the target class score
+    success_rate = np.sum(bigger_than_target_pert.any(1))/len(targets_orig)
+    print("success rate:", success_rate)
 
-    #calculate how many classes are bigger than target
-    number_of_bigger_classes=np.sum(bigger_than_target_pert, axis=1)
+    # calculate confidence (average probability of target classes)
+    confidence = np.sum(np.max(scores_of_pert_images.detach().numpy(), axis=0))/len(target_scores_in_pert_pred)
+    print("confidence: ", confidence)
+
+    # calculate how many classes are bigger than target
+    number_of_bigger_classes = np.sum(bigger_than_target_pert, axis=1)
+    #print("number of more probable classes: ", number_of_bigger_classes)
+
+    print("bigger_than.shape: ",bigger_than_target_pert.shape)
 
     return success_rate, confidence, number_of_bigger_classes
 
@@ -288,11 +415,11 @@ def NN_and_pixel():
 
 if __name__ ==  '__main__':
     #1
-    #reproduction()
+    reproduction()
 
     #2
     #optimize_population()
     #optimize_f_crit()
 
     #3
-    NN_and_pixel()
+    #NN_and_pixel()
